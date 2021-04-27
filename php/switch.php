973a3580c2dd7a4fc4a9b8f147b323b06862a9e3
igorbarrosunipar <barrosigor74@gmail.com>
2021-04-27 08:58:51 -0300

    switch

---
+++ php/switch.php
@@ -0,0 +1,17 @@
+<?php
+    $cor_favorita = "vermelho";
+
+    switch($cor_favorita) {
+        case "vermelho":
+            echo "sua cor favorita é vermelho";
+            break;
+        case "azul":
+            echo "sua cor favorita é azul";
+            break;
+        case "verde":
+            echo "sua cor favorita é verde";
+            break;
+        default:
+            echo "você não tem cor favorita";
+    }
+?>
